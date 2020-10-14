1a0828d23a40f15671d09e6e4d8beda6539e4bb6
lenkton <antonov_s@yahoo.com>
2020-10-14 13:31:53 +0300

    Added floating-point input support

--- sq_roots.rb
+++ sq_roots.rb
@@ -1,17 +1,16 @@
 string = gets 
 string = string.split
-a = string[0].to_i
-b = string[1].to_i
-c = string[2].to_i
+a = string[0].to_f
+b = string[1].to_f
+c = string[2].to_f
 
 puts(d = b**2 - 4*a*c)
-if d < 0
-  puts "Корней нет"
-end
-if d == 0
+if (d - 0.0000001).abs < 0
   puts ((-b)/(2*a))
-end
-if d > 0
-  puts((d**0.5 - b)/(2*a))
-  puts((-1*(d**0.5) - b)/(2*a))
+else
+  puts "Корней нет" if d < 0
+  if d > 0
+    puts((d**0.5 - b)/(2*a))
+    puts((-1*(d**0.5) - b)/(2*a))
+  end
 end
